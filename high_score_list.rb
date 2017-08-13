192f23df0dc8866391327b942ffb7816efe24e81
StarBug-1 <30919853+StarBug-1@users.noreply.github.com>
2017-08-14 09:55:45 +1000

    High score list update (not complete)

--- high_score_list.rb
+++ high_score_list.rb
@@ -1,12 +1,18 @@
+require 'terminal-table'
+require 'date'
+
 class HighScoreList
-  attr_accessor :scores
-  
+  attr_accessor :scores :table
+
+  table = Terminal::Table.new :title => "Coder Academy - The Game: High Scores", :headings => ['Player Name', 'Date', 'Total Score'], :rows => rows
+
   def initialize
-    @scores = []
+      @scores = []
+    end
   end
 
-  def add_score(player)
-    @scores << player
+  def add_score(player, total_score)
+    @scores << [[player],[total_score],[Date.today]]
   end
 
   # def update_yaml
@@ -14,5 +20,9 @@ class HighScoreList
 
   # def publish
   # end
+
+      if File.file?('high_score_list.yaml')
+      @scores = YAML.load_file('high_score_list.yaml')
+    else
 
 end
