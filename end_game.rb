3e6d11a423f203ee1c0f0bb27fc93284479e7718
Simbidion <simonadwyer@yahoo.com.au>
2017-08-13 19:52:08 +1000

    some end of game bits

--- end_game.rb
+++ end_game.rb
@@ -1,5 +1,11 @@
 def end_of_game
+  current_year = Time.new.year
+  credits = "\nASCII art courtesy of Andreas Freise: (http://www.ascii-art.de) \nComputer scientist biographies courtesy of Wikiedia (https://en.wikipedia.org)\nThis program was created in Ruby for the course requirements for Coder Academy Fast Track. It was programmed by: SImon Dwyer, Nathaniel Mether, George, Nisval"
+
   system 'clear'
-  puts "This is the end of the game.... in end_game.rb"
+  puts credits
+  puts "\nEpilogue...What became of the class of #{current_year}?"
+  puts "\nThis is the end of the game.... in end_game.rb"
   gets
 end
+
